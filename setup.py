25534c6bce14d462475b4ff7be115e05ef07af4f
Andreas Kaiser <disko@binary-punks.com>
2015-10-15 11:56:51 +0200

    Update all requirements to their latest respective version.  Also add compatibility with recent pip versions.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ sqlalchemy-geonames
 
 """
 from __future__ import print_function
+import pip
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
@@ -27,7 +28,9 @@ setup(
     long_description=__doc__,
     packages=find_packages(exclude=('sqlalchemy_geonames.tests', )),
     install_requires=[str(ir.req) for ir
-                      in parse_requirements('requirements.txt')],
+                      in parse_requirements(
+                        'requirements.txt',
+                        session=pip.download.PipSession())],
     entry_points={
         'console_scripts': {
             'sqlageonames = sqlalchemy_geonames.bin.sqlageonames:main',
